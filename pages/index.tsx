1b2339d86228e01cf8d6b9a2328de6bdfc6ac619
KiriLL4101 <fedechkin-kiril@mail.ru>
2022-12-27 08:42:38 +0300

    add types

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,40 @@
+import { GetStaticProps } from "next";
+import axios from "axios";
+
 import { Button } from "../components";
 import Rating from "../components/Rating/Rating";
 import { withLayout } from "../layout/Layout";
 
-function Home() {
+export enum TopLevelCategory {
+  Courses,
+  Services,
+  Books,
+  Products,
+}
+
+export interface PageItem {
+  alias: string;
+  title: string;
+  _id: string;
+  category: string;
+}
+
+export interface MenuItem {
+  _id: {
+    secondCategory: string;
+  };
+  isOpened?: boolean;
+  pages: PageItem[];
+}
+
+export interface FirstLevelMenuItem {
+  route: string;
+  name: string;
+  icon: JSX.Element;
+  id: TopLevelCategory;
+}
+
+function Home({ menu, firstCategory }: HomeProps) {
   return (
     <>
       <main>
@@ -16,3 +48,26 @@ function Home() {
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+
+  const { data: menu } = await axios.post<MenuItem[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/find",
+    {
+      firstCategory,
+    }
+  );
+
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  };
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
